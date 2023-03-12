4bb9eb88b564e219bd85286900bbe3b5c04bd93d
Maikoncarlos <maikonsaturno@gmail.com>
2023-03-12 18:55:14 -0300

    Conceitos de Spring Boot - criando projeto base

---
+++ src/main/java/com/github/maikoncarlos/aws_project1_started/AwsProject1StartedApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.aws_project1_started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProject1StartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProject1StartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maikoncarlos/aws_project1_started/AwsProject1StartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.aws_project1_started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject1StartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
